1ca42d00042bef13fd31252c80d793f69ed5bcd5
Kammal Shetty Sai Sumanth <sumanthksai@gmail.com>
2023-01-30 23:51:38 -0500

    commiting assign-1

---
+++ services/AuthService.js
@@ -0,0 +1,59 @@
+const { User } = require('../models');
+const bcrypt = require("bcrypt");
+
+const BasicAuth = async (req, res, next) => {
+    console.log(req.headers);
+    // check if authorization header is present
+    if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+        return res.status(401).json({
+            message: 'Missing Authorization Header'
+        })
+    }
+    // verify auth credentials
+    const base64Credentials = req.headers.authorization.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    //whatif password contains colon
+    const [email, password] = credentials.split(':');
+
+    //get user with emailid
+    //TODO: Verify with TA??
+
+    const user = await User.findOne({ where: { username: email } });
+    console.log(user);
+    if (!user) {
+        return res.status(400).json({
+            message: 'Bad Request'
+        })
+    }
+    // verify password
+    // const isPasswordMatch = await user.password === password;
+    const isPasswordMatch = bcrypt.compareSync(password, user.password)
+    if (!isPasswordMatch) {
+        return res.status(401).json({
+            message: 'Invalid Authentication Credentials'
+        }),
+            console.log("Password not match");
+    }
+    // verify if user is trying to access his own account
+    if (req.params.userID){
+        console.log("UID:::" +user.id +" " +req.params.userID);
+        if (user.id != req.params.userID) {
+            return res.status(403).json({
+                message: 'Forbidden Resource'
+            }),
+                console.log("User not match");
+        }
+    }
+    else{
+        return res.status(400).json({
+            message: 'Bad Request'
+        }) 
+    }
+    // authentication successful
+    next();
+
+
+
+}
+
+module.exports = BasicAuth;
---
+++ services/userService.js
@@ -0,0 +1,108 @@
+const {User} = require('../models');
+const validate = require('./validation')
+var bcrypt = require('bcryptjs');
+var message = require('../constant/messages');
+
+var userService = {}
+
+userService.createUser = async function(data,func){
+    var errors = validate.createErrorObj(data);
+    if(errors.length>0){
+        var errorObj = {
+            isSuccess : false,
+            errors : errors
+        }
+       return func(errorObj,null,400);
+    }
+   data.password= encryptPassword(data.password);
+   const {username, password,first_name,last_name} = data;
+   try{
+       
+        const user = await User.create({username, password,first_name,last_name});
+        func(null,user,201) ;
+    }
+    catch(err){
+        func(validate.createErrorObj(err),null,400) ;
+    }
+}
+
+userService.updateUser = async function(data,func){
+    try{
+        if(!data.body){
+            return  func(validate.errorObj(message.INVALID_BODY),null,400);
+        }
+        if(data.body.account_created_at){
+            return  func(validate.errorObj(message.NO_ACC_CREATED_AT),null,400);
+        }
+        if(data.body.account_updated_at){
+            return  func(validate.errorObj(message.NO_ACC_UPDATED_AT),null,400);
+        }
+        if(!data.body.first_name && !data.body.last_name && !data.body.password){
+            return func(validate.errorObj(message.NO_DATA_UPDATE),null,400);
+        }
+        const userID = data.params.userID;
+        var userData = await User.findByPk(userID);
+        if(data.body.username && userData.username!=data.body.username){
+            return  func(validate.errorObj(message.NO_EMAIL_ADDRESS),null,400);
+        }
+        if(data.body.id && userData.id!=data.body.id){
+            return  func(validate.errorObj(message.NO_ID_UPDATE),null,400);
+        }
+        var param = {};
+        if(data.body.first_name){
+            param.first_name= data.body.first_name
+        }
+        if(data.body.last_name){
+            param.last_name= data.body.last_name
+        }
+        if(data.body.password){
+            param.password = encryptPassword(data.body.password);
+        }
+        const savedObj = await User.update(param, { where: { id:userID } });
+        if(savedObj){
+            const message ="successfully updated";
+            return func(null,{message},204);
+        }
+        else{
+            return  func(validate.errorObj(message.NO_UPDATE),null,400);
+        }
+    }
+    catch(error){
+        console.log(error);
+        return func(error,null,400);
+    }
+
+
+};
+userService.getUserDetails = async function(data,func){
+    try{
+        var id = data.params.userID;
+        const userObj = await User.findByPk(id);
+        if(!userObj){
+            return func(validate.errorObj(message.NO_RECORD),null,400);
+        }
+        const res ={
+            id: userObj.id,
+            username:userObj.username,
+            first_name:userObj.first_name,
+            last_name:userObj.last_name,
+            account_created:userObj.account_created,
+            account_updated:userObj.account_updated
+        }
+        return func(null,res,200);
+    }
+    catch(error){
+        return func(error,null,400);
+    }
+
+}
+
+function encryptPassword(password){
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+    return hash;
+}
+function comparePassword(password,hash){
+    return bcrypt.compareSync(password, hash);
+}
+ module.exports = userService;
