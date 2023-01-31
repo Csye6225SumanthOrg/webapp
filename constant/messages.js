9452a7bf4139a0b083d75ce6ecc58d4eeb604a0b
Kammal Shetty Sai Sumanth <sumanthksai@gmail.com>
2023-01-31 12:48:09 -0500

    adding assign files

---
+++ constant/messages.js
@@ -0,0 +1,18 @@
+module.exports ={
+    EMAIL_ALREADY_PRESENT: "102: You have already registered with this email ID.",
+    EMAIL_NOT_PRESENT: "120: Email is not present",
+    INVALID_EMAIL: "101: Enter valid Email",
+    INVALID_PASSWORD: "102: Your Password should contain minimum eight characters, at least one letter, one number and one special character",
+    INVALID_NAME: "103: Enter valid name",
+    NO_USER_FOUND: "101: No user found",
+    INCORRECT_PASSWORD: "230: Please enter correct password",
+    INVALID_BODY:"Payload can't be empty",
+    NO_ACC_UPDATED_AT:" account_updated_at cannot be updated",
+    NO_ACC_CREATED_AT:" account_created_at cannot be updated",
+    NO_EMAIL_ADDRESS:"Email Address cannot be updated",
+    NO_DATA_UPDATE:" Payload doesn't contains any of first_name,secondName,password for update",
+    NO_ID_UPDATE:"ID cannot be update",
+    NO_UPDATE:"Couldn't update the record",
+    NO_RECORD:"No record found"
+
+}
---
+++ route/userRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const UserController = require('../controller/userController')
+const AuthService = require('../services/AuthService');
+const route = express.Router();
+
+
+route.post('/v1/user',[UserController.createUser]);
+route.put('/v1/user/:userID', [AuthService,UserController.updateUser]);
+route.get('/v1/user/:userID', [AuthService,UserController.getUser]);
+route.get('/healthz',[UserController.health])
+route.get('/sampleAPI',[UserController.sampleAPI]);
+
+
+module.exports = route;
