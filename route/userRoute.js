adcd9dd03fed3b0f811361e89e7f91747898cfb5
Kammal Shetty Sai Sumanth <sumanthksai@gmail.com>
2023-01-31 11:04:58 -0500

    workflow bug test

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const UserController = require('../controller/userController')
 const AuthService = require('../services/AuthService');
+const UserController = require('../controller/userController')
+//const AuthService = require('../services/AuthService');
 const route = express.Router();
 
 
