20e4ffbb73f62e15f4570ed7143df8565529960f
Kammal Shetty Sai Sumanth <sumanthksai@gmail.com>
2023-03-20 17:34:07 -0400

    cloudwatch-config changes

--- test/test.js
+++ test/test.js
@@ -4,15 +4,6 @@ const app = require('../app')
 
 
 describe('Testing our Application', function() {
-    it('GET /healthz end point of the application', (done) => {
-        supertest(app)
-            .get('/healthz')
-            .expect(200)
-            .end((err, response) => {
-                if (err) return done(err)
-                return done()
-            })
-    })
     it('GET 404 API endpoint', (done) => {
         supertest(app)
             .get('/notfoundendpoint')
